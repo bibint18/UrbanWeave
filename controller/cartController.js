77b03cf3e2cbd705639e51858a8a17e318927c78
Bibin T <bibint2918@gmail.com>
2024-10-17 21:04:26 +0530

    minor changes in cart

--- controller/cartController.js
+++ controller/cartController.js
@@ -4,16 +4,12 @@ const Product = require('../model/admin/prodectModel')
 exports.getCart =async (req,res) => {
   try {
     const userId = req.user.id
-    console.log("usre: ",userId)
-    const cartItems = await Cart.find({user:userId}).populate('product').exec()
-    console.log("items: ",cartItems);
-    
+    const cartItems = await Cart.find({user:userId}).populate('product').exec()  
     if(!cartItems || cartItems.length ==0){
       return res.render('user/cart',{cartItems:[],message:"Cart is empty"})
     }
     const itemsWithTotal = cartItems.map((item) => {
       const total = item.quantity * item.product.salePrice;
-      console.log("total",total);
       return{
         ...item.toObject(),total
       }
@@ -32,13 +28,33 @@ exports.AddCart =async (req,res) => {
     // console.log("req,",req.user)
     const userId = req.user.id
     const{size,productId,quantity} = req.body 
+    const requestedQuantity = parseInt(quantity,10)
+
     const product = await Product.findById(productId)
     if(!product){
       return res.status(400).json({success:false,message:"product not found"})
     }
+
+    const sizeStock = await product.sizes.find(s => s.size ===size)
+    if(!sizeStock){
+      return  res.status(400).json({success:false,message:"size not found"})
+    }
+    if(requestedQuantity > sizeStock.stock){
+      return res.status(400).json({ success: false, message: "Not enough stock available" });
+    }
+    if(requestedQuantity > 5){
+      return res.status(400).json({ success: false, message: "Maximum quantity is 5"})
+    }
+
+
     let cartItem = await Cart.findOne({user:userId,product:productId,size:size})
     if(cartItem){
-      cartItem.quantity += parseInt(quantity,10)
+      // cartItem.quantity += parseInt(quantity,10)
+      const newQuantity = cartItem.quantity + requestedQuantity
+      if(newQuantity > sizeStock.stock || newQuantity > 5){
+        return res.status(400).json({ success: false, message: "Exceeds stock or max limit" })
+      }
+      cartItem.quantity = newQuantity;
       await cartItem.save()
     }else{
       cartItem=new Cart({
@@ -49,6 +65,8 @@ exports.AddCart =async (req,res) => {
       })
       await cartItem.save()
     }
+    sizeStock.stock -= requestedQuantity;
+    await product.save();
     return res.status(200).json({success:true,message:"added"})
   } catch (error) {
     console.log(error)
@@ -61,6 +79,7 @@ exports.deleteCart = async (req,res) => {
     const id = req.params.id
     const deleteItem = await Cart.findByIdAndDelete(id)
     if(!deleteItem){
+      console.log("no item")
       return res.status(400).json({success:false,message:"item not found"})
     }
     return res.status(200).json({success:true,message:"deleted"})
